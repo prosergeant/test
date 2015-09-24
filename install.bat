bea6216bf0898a3de02cdb13970a8f56129ff412
prosergeant <sberendeyev@gmail.com>
2015-09-25 00:30:49 +0600

    install includes for win

---
+++ install.bat
@@ -0,0 +1,5 @@
+cd C:\test
+xcopy "C:\test\iniparser" "C:\program files (x86)\mingw\include\iniparser\" /S /E
+xcopy "C:\test\common" "C:\program files (x86)\mingw\include\common\" /S /E
+xcopy "C:\test\glm" "C:\program files (x86)\mingw\include\glm\" /S /E
+pause
