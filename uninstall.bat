31f5c447855d8a0f11223252c9318413c232ceaf
prosergeant <sberendeyev@gmail.com>
2015-09-25 00:31:32 +0600

    uninstall includes for win

---
+++ uninstall.bat
@@ -0,0 +1,5 @@
+cd C:\test
+del "C:\program files (x86)\mingw\include\iniparser\" /S /E
+del "C:\program files (x86)\mingw\include\common\" /S /E
+del "C:\program files (x86)\mingw\include\glm\" /S /E
+pause
