ce9a0da673daa23e1b130f899be6dd488ee9418c
prosergeant <sberendeyev@gmail.com>
2015-09-25 11:45:39 +0600

    some fix

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
-cd C:\test
-xcopy "C:\test\iniparser" "C:\program files (x86)\mingw\include\iniparser\" /S /E
-xcopy "C:\test\common" "C:\program files (x86)\mingw\include\common\" /S /E
-xcopy "C:\test\glm" "C:\program files (x86)\mingw\include\glm\" /S /E
+cd "C:\users\oleg b\test"
+xcopy "C:\users\oleg b\test\iniparser" "C:\program files (x86)\mingw\include\iniparser\" /S /E
+xcopy "C:\users\oleg b\test\common" "C:\program files (x86)\mingw\include\common\" /S /E
+xcopy "C:\users\oleg b\test\glm" "C:\program files (x86)\mingw\include\glm\" /S /E
 pause
